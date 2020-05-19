4939facbb50e7e1509340c2809d4529e04c8090b
Claudiu Chis <claudiu.chis@gmail.com>
2020-05-19 23:29:37 +0100

    updated version of bootstrap-vue

--- web/serverless-microservices-web/public/js/settings.js
+++ web/serverless-microservices-web/public/js/settings.js
@@ -1,13 +1,13 @@
 // Auth
-window.authClientId = '';
-window.authAuthority = '';
-window.authScopes = [''];
-window.authEnabled = false;
+window.authClientId = '0f8f9adc-6f4b-4960-a59d-bb31f73ff4d3';
+window.authAuthority = 'https://hailoTenant.b2clogin.com/tfp/hailoTenant.onmicrosoft.com/b2c_1_hailo_default_signin/v2.0';
+window.authScopes = ['rideshare'];
+window.authEnabled = true;
 
 // API endpoints
-window.apiKey = '';
-window.apiBaseUrl = '';
+window.apiKey = 'c773dc362db840b7a4e3bae8139c10ed';
+window.apiBaseUrl = 'https://hailo-apim.azure-api.net';
 window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
 window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
 window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
-window.signalrInfoUrl = '';
+window.signalrInfoUrl = 'hailotripfunctionapp.azurewebsites.net/api/signalrinfo';
